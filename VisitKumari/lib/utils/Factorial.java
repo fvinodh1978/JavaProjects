f48c77b1a6f516ff695fef5cb356833237e990aa
fvinodh1978 <f.vinodhfranklin@gmail.com>
2023-05-13 23:29:53 +0530

    [ITRACK001] Added new Project VisitKumari

---
+++ VisitKumari/lib/utils/Factorial.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Factorial {
+    public static int factorial(int number) {
+        if (number <= 1) {
+            return 1;
+        }
+        return number * factorial(number - 1);
+    }
+
+    public static void main(String[] args) {
+        int value=0;
+        int number=0;
+        
+        try{
+            Scanner getInput = new Scanner(System.in)
+            System.out.print("Enter name, age and salary:");
+            number=getInput.nextInt();
+            value=factorial(number);
+        }catch(InputMismatchException e){
+            System.out.println("Value Entered is not an Integer, Please try again");
+        }
+        System.out.println("Factorial :" + value);
+    }
+}
