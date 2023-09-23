c36578ca4a67a4918954c2c3d318a3ac469042d8
Vinodh Francis <f.vinodhfranklin@gmail.com>
2023-09-23 10:33:10 +0530

    Added Excel Utils

--- Utilities/ExcelUtils.java
+++ Utilities/ExcelUtils.java
@@ -3,81 +3,115 @@ package examples;// Java Program to Illustrate Reading
 
 // Import statements
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 // Main class
 public class ExcelUtils {
     // Main driver method
     public static void main(String[] args) {
         ExcelUtils eu = new ExcelUtils();
-        String inputFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users.xlsx";
+        String inputFile = "";
+        String outFile = "";
+
+        // Read Excel File
+        inputFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users.xlsx";
 //        eu.readxlsx(inputFile);
 
-        InputStream inp = null;
+        // Convert Excel to Binary File
+        inputFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users.xlsx";
+        outFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users.bin";
+//        eu.convertExcelToBin(inputFile, outFile);
+
+        // Convert Binary to Excel File
+        inputFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users.bin";
+        outFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users1.xlsx";
+//        eu.convertBinaryToExcel(inputFile, outFile);
+
+        // Convert Excel to CSV File
+        inputFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\Users.xlsx";
+        outFile = "C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources";
+        eu.convertExcelToCSV(inputFile, outFile);
+    }
+
+    public void convertExcelToCSV(String inputFile, String outFile) {
+        FileInputStream inp = null;
+        int colCount = 0;
+        int rowCount = 0;
         try {
             inp = new FileInputStream(inputFile);
             XSSFWorkbook wb = new XSSFWorkbook(inp);
 
             for (int i = 0; i < wb.getNumberOfSheets(); i++) {
                 System.out.println(wb.getSheetAt(i).getSheetName());
-                eu.convertExcelToCSV(wb.getSheetAt(i), wb.getSheetAt(i).getSheetName());
+                XSSFSheet sheet = wb.getSheetAt(i);
+                String sheetName = wb.getSheetAt(i).getSheetName();
+
+                StringBuffer data = new StringBuffer();
+                FileOutputStream fos = new FileOutputStream(outFile + "\\" + sheetName + ".csv");
+                Row row;
+                colCount = getColumnsCount(sheet);
+                System.out.printf("Number of Columns = %d\n", colCount);
+                Iterator<Row> rowIterator = sheet.iterator();
+                rowCount = 0;
+                while (rowIterator.hasNext()) {
+                    row = rowIterator.next();
+                    row.getLastCellNum();
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    System.out.printf("Current Row = %d\n", rowCount);
+//                    while (cellIterator.hasNext()) {
+                    for (int j = 0; j < colCount; j++) {
+                        Cell cell = row.getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                        System.out.printf("Current cell = %d\n", j);
+//                        cell = cellIterator.next();
+
+                        if (!(cell == null)) {
+                            CellType type = cell.getCellType();
+
+                            System.out.println("Current cell Type = " + type);
+                            if (type == CellType.BOOLEAN) {
+                                System.out.printf("Current cell = %s\n", cell.getBooleanCellValue());
+                                data.append(cell.getBooleanCellValue() + ",");
+                            } else if (type == CellType.NUMERIC) {
+                                System.out.printf("Current cell = %f\n", cell.getNumericCellValue());
+                                data.append(cell.getNumericCellValue() + ",");
+                            } else if (type == CellType.STRING) {
+                                System.out.printf("Current cell = %s\n", cell.getStringCellValue());
+                                data.append(cell.getStringCellValue() + ",");
+                            } else if (type == CellType.BLANK) {
+                                System.out.println("In Blank");
+                                data.append(",");
+                            } else if (cell == null || cell.getStringCellValue() == "") {
+                                System.out.println("In Null");
+                                data.append(",");
+                            } else {
+                                System.out.println("In Else");
+                                data.append(cell + ",");
+                            }
+                        }
+                    }
+                    data.append('\n');
+                    rowCount++;
+                }
+                fos.write(data.toString().getBytes());
+                fos.close();
             }
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            System.out.println(ex);
         } finally {
             try {
                 inp.close();
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
-        }
-    }
-
-    public void convertExcelToCSV(XSSFSheet sheet, String sheetName) {
-        StringBuffer data = new StringBuffer();
-        try {
-            FileOutputStream fos = new FileOutputStream("C:\\Users\\054544744\\IdeaProjects\\FST Java\\src\\main\\resources\\" + sheetName + ".csv");
-            Cell cell;
-            Row row;
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                row = rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while (cellIterator.hasNext()) {
-                    cell = cellIterator.next();
-
-                    CellType type = cell.getCellType();
-                    if (type == CellType.BOOLEAN) {
-                        data.append(cell.getBooleanCellValue() + ",");
-                    } else if (type == CellType.NUMERIC) {
-                        data.append(cell.getNumericCellValue() + ",");
-                    } else if (type == CellType.STRING) {
-                        data.append(cell.getStringCellValue() + ",");
-                    } else if (type == CellType.BLANK ) {
-                        data.append(",");
-                    }else if(cell==null || cell.getStringCellValue() == "") {
-                        data.append(",");
-                    } else {
-                        data.append(cell + ",");
-                    }
-                }
-                data.append('\n');
-            }
-            fos.write(data.toString().getBytes());
-            fos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -137,5 +171,103 @@ public class ExcelUtils {
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    public void convertExcelToBin(String inputFile, String outFile) {
+        try {
+            // Load the Excel file
+            FileInputStream excelFile = new FileInputStream(new File(inputFile));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+
+            // Create a binary output stream
+            FileOutputStream binaryFile = new FileOutputStream(outFile);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(binaryFile);
+
+            // Iterate through each sheet and write its content to the binary file
+            for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+                Sheet sheet = workbook.getSheetAt(i);
+                for (Row row : sheet) {
+                    for (Cell cell : row) {
+                        objectOutputStream.writeObject(cell.toString().getBytes());
+                    }
+                }
+            }
+
+            // Close the streams
+            objectOutputStream.close();
+            binaryFile.close();
+
+            System.out.println("Excel file converted to binary file successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void convertBinaryToExcel(String inputFile, String outFile) {
+        try {
+            // Create a new Excel workbook
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Sheet1");
+
+            // Open the binary input stream
+            FileInputStream binaryFile = new FileInputStream(inputFile);
+            ObjectInputStream objectInputStream = new ObjectInputStream(binaryFile);
+
+            int rowNum = 0;
+            while (true) {
+                try {
+                    // Read binary data from the input stream
+                    byte[] bytes = (byte[]) objectInputStream.readObject();
+
+                    // Create a new row and cell
+                    Row row = sheet.createRow(rowNum++);
+                    Cell cell = row.createCell(0);
+
+                    // Set the cell value from the binary data
+                    cell.setCellValue(new String(bytes));
+                } catch (EOFException e) {
+                    // End of file reached
+                    break;
+                }
+            }
+
+            // Close the input stream
+            objectInputStream.close();
+
+            // Write the Excel workbook to a file
+            FileOutputStream excelFile = new FileOutputStream(outFile);
+            workbook.write(excelFile);
+            excelFile.close();
+
+            System.out.println("Binary file converted to Excel file successfully.");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Count max number of nonempty cells in sheet rows
+    public int getColumnsCount(XSSFSheet xssfSheet) {
+        int result = 0;
+        Iterator<Row> rowIterator = xssfSheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            List<Cell> cells = new ArrayList<>();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                cells.add(cellIterator.next());
+            }
+            result = Math.max(cells.size(), result);
+
+//            for (int i = cells.size(); i >= 0; i--) {
+//                Cell cell = cells.get(i-1);
+//                if (cell.toString().trim().isEmpty()) {
+//                    cells.remove(i-1);
+//                } else {
+//                    result = cells.size() > result ? cells.size() : result;
+//                    break;
+//                }
+//            }
+        }
+        return result;
     }
 }
